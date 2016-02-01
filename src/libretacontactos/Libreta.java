8b4d29d573cda3aa660cc528521d987c6ced2b4e
Jota <Jota@JotaQuery-P>
2016-02-01 09:30:44 +0100

    Finalizado

---
+++ src/libretacontactos/Libreta.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libretacontactos;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Jota
+ */
+public class Libreta {
+    //private String nombre;
+    //private String dni;
+    private final Contacto[] contactos=new Contacto[10];
+    private final Grupo[] grupos = new Grupo[10];
+    
+    public Libreta(){
+        /*for(int i=0;i<contactos.length;i++){
+            contactos[i]=new Contacto();
+            contactos[i].setNombre("");
+        }*/
+    }
+    
+    public void engadir(Contacto contacto){
+        boolean libretaLlena=true;
+        for(int i=0;i<contactos.length;i++){
+            if(contactos[i]==null){
+                contactos[i]=contacto;
+                libretaLlena=false;
+                break;
+            }
+        }
+        
+        if(libretaLlena){
+            JOptionPane.showMessageDialog(null, "Libreta llena");
+        }
+    }
+    
+    public void engadir(){
+        boolean libretaLlena=true;
+        for(int i=0;i<contactos.length;i++){
+            if(contactos[i]==null){
+                Contacto contacto = new Contacto(JOptionPane.showInputDialog("Nombre:"),JOptionPane.showInputDialog("Apellidos:"));
+                contactos[i]=contacto;
+                libretaLlena=false;
+                break;
+            }
+        }
+        
+        if(libretaLlena){
+            JOptionPane.showMessageDialog(null, "Libreta llena");
+        }
+    }
+    
+    public void listar(){
+        String cadena="";
+        for(Contacto elemento:contactos){
+            if(elemento!=null){
+                cadena+=elemento.toString()+"\n";
+            }
+        }
+        JOptionPane.showMessageDialog(null,cadena);
+    }
+    
+    public void borrar(){
+        boolean notFound = true;
+        int id = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el id del contacto que desea borrar:"));
+        for(int i=0;i<contactos.length;i++){
+            if(contactos[i]!=null){
+                if(contactos[i].getId()==id){
+                    contactos[i]=null;
+                    notFound=false;
+                    break;
+                }
+            }
+        }
+        if(notFound){
+            JOptionPane.showMessageDialog(null, "Contacto no encontrado");
+        }
+    }
+    
+    public void crearGrupo(){
+        boolean grupoLleno = true;
+        String nombre = JOptionPane.showInputDialog("Introduce el nombre del grupo");
+        Grupo grupo = new Grupo(nombre);
+        for(int i=0;i<grupos.length;i++){
+            if(grupos[i]==null){
+                grupos[i]=grupo;
+                grupoLleno=false;
+                break;
+            }
+        }
+        if(grupoLleno){
+            JOptionPane.showMessageDialog(null, "Límite de grupos alcanzado");
+        }
+    }
+    
+    public void agregarAGrupo(){
+        boolean contactoEncontrado = false ,grupoEncontrado = false;
+        int indiceContactos = 0, indiceGrupos=0;
+        int id = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el id del contacto que quiere guardar en el grupo:"));
+        String nombreGrupo = JOptionPane.showInputDialog("Nombre del grupo en el que quiere guardar el contacto:");
+        for(int i=0;i<contactos.length;i++){
+            if(contactos[i]!=null){
+                if(contactos[i].getId()==id){
+                    indiceContactos=i;
+                    contactoEncontrado=true;
+                    break;
+                }
+            }
+        }
+        
+        for(int i=0;i<grupos.length;i++){
+            if(grupos[i]!=null){
+                if(grupos[i].getNombre().equalsIgnoreCase(nombreGrupo)){
+                    indiceGrupos=i;
+                    grupoEncontrado=true;
+                    break;
+                }
+            }
+        }
+        
+        if(contactoEncontrado && grupoEncontrado){
+            contactos[indiceContactos].setGrupo(grupos[indiceGrupos]);
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Contacto o grupo no encontrado");
+        }
+    }
+}
---
+++ src/libretacontactos/MiApp.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libretacontactos;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jota
+ */
+public class MiApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Libreta libreta = new Libreta();
+        boolean condicion = true;
+        do{
+        int seleccion = Integer.parseInt(JOptionPane.showInputDialog("1)Engadir contacto\n2)Listar contactos\n3)Borrar contacto\n5)Crear grupo\n6)Añadir contacto a grupo\n7)Salir"));
+        switch(seleccion){
+            case 1:
+                libreta.engadir();
+                break;
+            case 2:
+                libreta.listar();
+                break;
+            case 3:
+                libreta.borrar();
+                break;
+            case 5:
+                libreta.crearGrupo();
+                break;
+            case 6:
+                libreta.agregarAGrupo();
+                break;
+            case 7:
+                System.exit(0);
+            default:
+                JOptionPane.showMessageDialog(null, "Opción no válida");
+        }
+        }while(condicion);
+        
+        
+    }
+    
+}
